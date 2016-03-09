9fc23f237463f65e3e032969bc575b9c74453609
mamusi20f <mamusi20@gmail.com>
2016-03-09 19:12:30 +0900

    Added files via upload

---
+++ demo-openrc.sh
@@ -0,0 +1,10 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=devstack
+export OS_AUTH_URL=http://192.168.56.101:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+export PS1='\u@\h \W(demo)\$ '
